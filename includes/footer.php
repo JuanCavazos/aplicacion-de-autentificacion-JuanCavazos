6e611be7b6f6962dfec6df3b907ce31b1d578eda
JuanCavazos <juanalbertocavazosmontano83@gmail.com>
2024-03-01 21:16:36 -0600

    Se sube el código a GitHub
    Código hecho en clase

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+</head>
+<body>
+    
+</body>
+
+
+<!-- footer.php -->
+<footer>
+    <p>&copy; <?php echo date ("Y"); ?> Mi sitio Web</p>
+</footer>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <!--Titulo de la pagina-->
+    <title>Iniciar Sesión</title>
+</head>
+
+<body>
+    <!--Menú de navegación-->
+    <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="contraseñahash.php">Contraseña Hash</a></li>
+            <li><a href="registro">Registro</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
